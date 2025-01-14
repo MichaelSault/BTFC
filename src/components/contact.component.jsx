8012d88b98bd9d71c6b9bdd8bd86f0809169b445
Michael Sault <michael_sault@hotmail.com>
2025-01-13 23:43:03 -0500

    started designing the contact section

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -1,7 +1,8 @@
 import {Button} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react'
-import '../App.css'
+import { useState } from 'react';
+import '../App.css';
+import '../styles/contact.css';
 
 
 function Contact() {
@@ -9,9 +10,29 @@ function Contact() {
 
   return (
     <>
-        <div className="contactRow">
-            <h1 className="title">Contact Us</h1>
+      <div className='contactRow'>
+        <div className='ContactQuestionsCol'>
+            <div className='contactTitleSubCol'>
+                <h1 className='contactTitle'>Contact Us</h1>
+                <hr className='hrContact'/>
+                
+                <div className='contactInfoSubCol'>
+                  <h2>Tammy Mackenzie - Head Coach</h2>
+                  <h4>Junior Group - Ages 5 to 13</h4>
+                  <h4>Senior Distance Group - Highschool Age</h4>
+                  <br/>
+                  <h4>Email - tammymackenzie@bell.net</h4>
+                  <h4>Call - (905)-332-7072</h4>
+                  <br/>
+                  <h2>General Inquiries</h2>
+                  <h4>Email - president@burlingtontrackandfield.org</h4>
+                </div>
+            </div>
         </div>
+        <div className='contactPhotoCol'>
+          <h2 className='attireSubText'>Contact Us Image</h2>
+        </div>
+      </div>
                 
     </>
   )
