aa0b6e1d6bf0400dbb2675ed70419c0a3be88191
Michael Sault <michael_sault@hotmail.com>
2025-02-03 23:59:33 -0500

    adjusted layout of the contact us component

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -13,27 +13,26 @@ function Contact() {
   return (
     <div className="desktopOnly">
       <div className='contactRow'>
-        <div className='ContactQuestionsCol'>
-            <div className='contactTitleSubCol'>
-                <h2 className='contactTitle'>Contact Us</h2>
-                <hr className='hrContact'/>
-                
-                <div className='contactInfoSubCol'>
-                  <h3>Tammy Mackenzie - Head Coach</h3>
-                  <h6>Junior Group - Ages 5 to 13</h6>
-                  <h6>Senior Distance Group - Highschool Age</h6>
-                  <br/>
-                  <h6>Email - tammymackenzie@bell.net</h6>
-                  <h6>Call - (905)-332-7072</h6>
-                  <br/>
-                  <h3>General Inquiries</h3>
-                  <h6>Email - president@burlingtontrackandfield.org</h6>
-                </div>
-            </div>
-        </div>
+        
         <div className='contactPhotoCol'>
           <img src={ContactImage} />
           <h6 className='contactSubText'><i>Caption text for the above image.</i></h6>
+        </div>
+        <div className='contactQuestionsCol'>
+          <h2 className='contactTitle'>Contact Us</h2>
+          <hr className='hrContact'/>
+          
+          <div className='contactInfoSubCol'>
+            <h3>Tammy Mackenzie - Head Coach</h3>
+            <h6>Junior Group - Ages 5 to 13</h6>
+            <h6>Senior Distance Group - Highschool Age</h6>
+            <br/>
+            <h6>Email - tammymackenzie@bell.net</h6>
+            <h6>Call - (905)-332-7072</h6>
+            <br/>
+            <h3>General Inquiries</h3>
+            <h6>Email - president@burlingtontrackandfield.org</h6>
+          </div>
         </div>
       </div>
                 
