5ade96b78c3afac55482e0086522e3fba40d1c0b
Michael Sault <michael_sault@hotmail.com>
2025-02-08 16:03:59 -0500

    started initial layout for the registrations page

---
+++ src/components/registration.component.jsx
@@ -0,0 +1,15 @@
+import '../App.css';
+import '../styles/registration.css';
+
+function Registration() {
+
+  return (
+    <>
+        <div className="registrationRow desktopOnly">
+            <h1 className='registrationTitle'>REGISTRATION OPTIONS</h1>
+        </div>
+    </>
+  )
+}
+
+export default Registration
