3c2c087af4a4ce127631f11d9586e0f353f0506c
Michael Sault <michael_sault@hotmail.com>
2025-02-08 16:03:21 -0500

    redesigned programs landing page

--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -1,7 +1,7 @@
 import {Button} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useState } from 'react'
-import '../App.css'
+import { useState } from 'react';
+import '../App.css';
 
 //import page components
 import BannerHero from './banner.component.jsx';
@@ -13,7 +13,7 @@ import Contact from "./contact.component.jsx";
 import ContactMobile from "./contactMobile.component.jsx";
 import Directors from "./directors.component.jsx";
 import DirectorsMobile from "./directorsMobile.component.jsx";
-import Footer from "./socialFooter.component.jsx";
+import Registration from "./registration.component.jsx";
 
 function Home() {
   const navigate = useNavigate();
@@ -21,21 +21,9 @@ function Home() {
   return (
     <>
         <BannerHero />
-        <div className="introRow">
-          <h1><b>The Burlington Track and Field Club</b></h1>
-          <hr/>
-          <p>This will be a description or a brief history of the Burlington Track and Field club.  We have a long history of at least 30+ years but likely much longer.
-              We are a non-profit the has supported the community for the entire span of the clubs history.  Someone else will have to write this more professionally
-              but this will serve as some sort of placeholder.</p>
-        </div>
         <Programs/>
         <ProgramsMobile/>
-        <Results/>
-        <ResultsMobile/>
-        <Contact/>
-        <ContactMobile/>
-        <Directors/>
-        <DirectorsMobile/>
+        <Registration/>
     </>
   )
 }
--- src/components/programs.component.jsx
+++ src/components/programs.component.jsx
@@ -1,62 +1,20 @@
 import '../App.css';
-
-import ProgramCard from "./programCard.component";
-
-import ProgramCardLong from "./programCardLong.component";
-
-//card photos
-import junior from '../assets/lasalpark.png';
-import senior from '../assets/distance.jpeg';
-import sprint from '../assets/sprint.jpeg';
+import '../styles/programs.css';
 
 function Programs() {
 
   return (
     <>
         <div className="programsRow desktopOnly">
-          <hr/>
-          <h1>Available Programs</h1>
-          <div className="bodyProgramCards">
-            <div className="programsCardsRow">
-              <ProgramCardLong
-                photo={junior}
-                name="Junior (MTA)" 
-                age="6 to 13" 
-                distance="All" 
-                experience="All levels welcome" 
-                training="1-2 days per week" 
-                schedule="Indoor - Monday, Wednesday; Outdoor / XC - Monday, Thursday 6-7:30pm" 
-                races="Meets are on weekends throughout each season. Meet schedules are TBC" 
-                seasons="January to April: Indoor Training; May to August: Outdoor Track; September to December: Cross Country + Indoor Training"
-                url="Junior"
-              />
-              <ProgramCardLong
-                photo={senior}
-                name="Senior Distance (AO)" 
-                distance="Middle & Long 800m and up" 
-                age="High school age and up" 
-                experience="Should be able to run at least 4km." 
-                training="2-3 days per week" 
-                schedule="Monday and Wednesday evenings; Saturday mornings" 
-                races="Meets are on weekends throughout each season and some weekday evenings in summer." 
-                seasons="January to April: Indoor Training; May to August: Outdoor Track; September to December: Cross Country + Indoor Training"
-                url="Distance"
-              />
-              <ProgramCardLong
-                photo={sprint}
-                name="Senior Sprint (AO)" 
-                distance="Sprint up to 400m" 
-                age="High school age and up" 
-                experience="All levels welcome" 
-                training="2-3 days per week" 
-                schedule="Monday and Wednesday evenings; Saturday mornings" 
-                races="Meets are on weekends throughout each season and some weekday evenings in summer." 
-                seasons="January to April: Indoor Training; May to August: Outdoor Track; September to December: Outdoor + Indoor Training"
-                url="Sprint"
-              />
-            </div>
+          <h1 className='title'>BTC PROGRAMS</h1>
+          <p className='description'>The Burlington Track and Field Club offers a number of different programs depending on age and event distance.</p>
+          <div className='buttonRow'>
+            <a className='button'>JUNIOR</a>
+            <a className='button'>DISTANCE</a>
+            <a className='button'>SPRINT</a>
           </div>
-        </div>      
+            
+        </div>
     </>
   )
 }
