d774909f393fdc43cb27ae73cb8fb3ec2a827e4a
Michael Sault <michael_sault@hotmail.com>
2025-02-16 23:39:45 -0500

    built a rough draft of the contact page

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -1,42 +1,62 @@
-import {Button} from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
-import { useState } from 'react';
 import '../App.css';
-import '../styles/contact.css';
+import '../styles/registration.css';
 
-import ContactImage from '../assets/contact.png';
-
+import backgroundImage from '../assets/runningbanner.jpeg';
 
 function Contact() {
-  const navigate = useNavigate();
 
   return (
-    <div className="desktopOnly">
-      <div className='contactRow'>
-        
-        <div className='contactPhotoCol'>
-          <img src={ContactImage} />
-          <h6 className='contactSubText'><i>Caption text for the above image.</i></h6>
+    <>
+        <div className="registrationRow desktopOnly">
+            <div className='registrationCol'>
+                <h1 className='registrationTitle'>CONTACT US</h1>
+                <div className='disclaimerBox'>
+                    <p className='registrationDisclaimer'>
+                        <b>Tammy Mackenzie</b>
+                        <br/>
+                        <b><i>Head Coach</i></b>
+                        <br/>
+                        <b><i>Junior and Senior Distance Team</i></b>
+                        <br/>
+                        <b>Email: </b> tammymackenzie@bell.net
+                        <br/>
+                        <b>Call: </b> (905) 332-7072
+                        <br/>
+                        <br/>
+                        <b>General Inquiries:</b>
+                        <br/>
+                        <b>Email: </b> president@burlingtontrackandfield.org
+                    </p>
+                </div>
+            </div>
+            <div className='registrationCol'>
+                <div className='subRow'>
+                    <p className='registrationText'>
+                      We are a volunteer organization with the following aims and objectives:
+                      <br/>
+                      <br/>
+                      <ul>
+                        <li>
+                          To foster, promote and teach track and field within the city of Burlington and to provide maximum opportunities for all eligible individuals to participate regardless of ability.
+                        </li>
+                        <br/>
+                        <li>
+                          To develop and encourage sportsmanship and community spirit among all participates to the betterment of their physical, mental and social well-being.
+                        </li>
+                        <br/>
+                        <li>
+                          To promote and sponsor such activates as may contribute to the financial well-being of the club.
+                        </li>
+                        <br/>
+                        <li>
+                          We encourage you to try us out a couple of times before committing yourself to joining.
+                        </li>
+                      </ul>
+                    </p>
+                </div>
+            </div>
         </div>
-        <div className='contactQuestionsCol'>
-          <h2 className='contactTitle'>Contact Us</h2>
-          <hr className='hrContact'/>
-          
-          <div className='contactInfoSubCol'>
-            <h3>Tammy Mackenzie - Head Coach</h3>
-            <h6>Junior Group - Ages 5 to 13</h6>
-            <h6>Senior Distance Group - Highschool Age</h6>
-            <br/>
-            <h6>Email - tammymackenzie@bell.net</h6>
-            <h6>Call - (905)-332-7072</h6>
-            <br/>
-            <h3>General Inquiries</h3>
-            <h6>Email - president@burlingtontrackandfield.org</h6>
-          </div>
-        </div>
-      </div>
-                
-    </div>
+    </>
   )
 }
 
--- src/components/home.component.jsx
+++ src/components/home.component.jsx
@@ -6,10 +6,10 @@ import '../App.css';
 //import page components
 import BannerHero from './banner.component.jsx';
 import Programs from './programs.component.jsx';
-import ProgramsMobile from './programsMobile.component..jsx';
 import GordRusby from "./gordRusby.component.jsx";
 import KenLake from "./kenLake.component.jsx";
 import Registration from "./registration.component.jsx";
+import Contact from "./contact.component.jsx";
 
 function Home() {
   const navigate = useNavigate();
@@ -18,10 +18,10 @@ function Home() {
     <>
         <BannerHero />
         <Programs/>
-        <ProgramsMobile/>
         <Registration/>
         <GordRusby/>
         <KenLake/>
+        <Contact/>
     </>
   )
 }
