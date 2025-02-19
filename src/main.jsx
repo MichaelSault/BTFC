3bce4011d7868e698c20f3d837a243785844d31c
Michael Sault <michael_sault@hotmail.com>
2025-02-18 20:50:36 -0500

    switched to hashmaps to resolve gh-pages hosting issues

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, HashRouter } from 'react-router-dom';
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -15,6 +15,7 @@ import Distance from './components/programs/distance.component.jsx';
 import Sprint from './components/programs/sprint.component.jsx';
 
 import Programs from './components/programs.component.jsx';
+import Registration from './components/registration.component.jsx';
 import Contact from './components/contact.component.jsx';
 import Results from './components/results.component.jsx';
 import Directors from './components/directors.component.jsx';
@@ -27,11 +28,11 @@ ReactDOM.createRoot(document.getElementById('navBar')).render(
       <Container>
         <Navbar.Brand href="/">BTFC</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="programs">Programs</Nav.Link>
-          <Nav.Link href="register">Registration</Nav.Link>
-          <Nav.Link href="contact">Contact Us</Nav.Link>
-          <Nav.Link href="results">Results</Nav.Link>
-          <Nav.Link href="board">Board of Directors</Nav.Link>
+          <Nav.Link href="#/programs">Programs</Nav.Link>
+          <Nav.Link href="#/register">Registration</Nav.Link>
+          <Nav.Link href="#/contact">Contact Us</Nav.Link>
+          <Nav.Link href="#/results">Results</Nav.Link>
+          <Nav.Link href="#/directors">Board of Directors</Nav.Link>
         </Nav>
       </Container>
     </Navbar>
@@ -40,20 +41,20 @@ ReactDOM.createRoot(document.getElementById('navBar')).render(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/Junior' element={<Junior />}/>
         <Route path='/Distance' element={<Distance />}/>
         <Route path='/Sprint' element={<Sprint />}/>
         <Route path='/Programs' element={<Programs/>}/>
-        <Route path='/Register' element={<Programs/>}/>
+        <Route path='/Register' element={<Registration/>}/>
         <Route path='/Contact' element={<Contact/>}/>
         <Route path='/Results' element={<Results/>}/>
         <Route path='/Directors' element={<Directors/>}/>
       </Routes>
       <Footer/>
-    </BrowserRouter>
+    </HashRouter>
     
   </React.StrictMode>
 
