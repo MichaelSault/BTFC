6fa7f0840e30bb303f3665710e84cc8088d62313
Michael Sault <michael_sault@hotmail.com>
2025-02-23 00:00:20 -0500

    cleaned up text on results components

--- src/components/gordRusby.component.jsx
+++ src/components/gordRusby.component.jsx
@@ -8,7 +8,7 @@ function GordRusby() {
         <div className='grRow'>
             <div className='grCol'>
                 <h1 className='title'>GORD RUSBY MEMORIAL MEET</h1>
-                <p className='description'>Every year, the Burlington Track and Field Club hosts the Gord Rusby Memorial Meet during the Outdoor Track season.</p>
+                <p className='resultDescription'>Every year, the Burlington Track and Field Club hosts the Gord Rusby Memorial Meet during the Outdoor Track season.</p>
                 <a className='button'>RESULTS</a>
             </div>
             <div className='grCol2'>
--- src/components/kenLake.component.jsx
+++ src/components/kenLake.component.jsx
@@ -16,7 +16,7 @@ function KenLake() {
         </div>
         <div className='klCol'>
           <h1 className='title'>KEN LAKE MEMORIAL MEET</h1>
-          <p className='description'>Every year, the Burlington Track and Field Club hosts the Ken Lake Memorial Meet during the Cross Country season.</p>
+          <p className='resultDescription'>Every year, the Burlington Track and Field Club hosts the Ken Lake Memorial Meet during the Cross Country season.</p>
           <a className='button'>RESULTS</a>
         </div>
       </div>
