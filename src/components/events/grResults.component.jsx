d77db1f8615bc65b405e61c8ef5fe035b3d11753
Michael Sault <michael_sault@hotmail.com>
2025-03-17 23:01:50 -0400

    added result links for each year

--- src/components/events/grResults.component.jsx
+++ src/components/events/grResults.component.jsx
@@ -18,21 +18,21 @@ function grResults() {
           <h4 className="grResultSubText">TRACK AND FIELD</h4>
         </div>
         <div className='grResultCol'>
-          <a className='grResultButton'>2024 RESULTS</a>
-          <a className='grResultButton'>2023 RESULTS</a>
-          <a className='grResultButton'>2022 RESULTS</a>
-          <a className='grResultButton'>2019 RESULTS</a>
-          <a className='grResultButton'>2018 RESULTS</a>
+          <a href='https://sportmadesimple.com/results/GordRusby2024/index.html' className='grResultButton'>2024 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_61e44dcc349b4c3c908ded2e6eeb147b.pdf' className='grResultButton'>2023 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_407dbf51336f4f5b88b4f69ede6c3e4d.pdf?index=true' className='grResultButton'>2022 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_e2c69f3e0feb43e588ae61f0c6a5af98.pdf?index=true' className='grResultButton'>2019 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_809f9ce44a034f1da43669c57dbe2bde.pdf?index=true' className='grResultButton'>2018 RESULTS</a>
         </div>
       </div>
 
       <div className='grRow mobileOnly'>
         <div className='grResultCol'>
-          <a className='grResultButton'>2024 RESULTS</a>
-          <a className='grResultButton'>2023 RESULTS</a>
-          <a className='grResultButton'>2022 RESULTS</a>
-          <a className='grResultButton'>2019 RESULTS</a>
-          <a className='grResultButton'>2018 RESULTS</a>
+          <a href='https://sportmadesimple.com/results/GordRusby2024/index.html' className='grResultButton'>2024 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_61e44dcc349b4c3c908ded2e6eeb147b.pdf' className='grResultButton'>2023 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_407dbf51336f4f5b88b4f69ede6c3e4d.pdf?index=true' className='grResultButton'>2022 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_e2c69f3e0feb43e588ae61f0c6a5af98.pdf?index=true' className='grResultButton'>2019 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_809f9ce44a034f1da43669c57dbe2bde.pdf?index=true' className='grResultButton'>2018 RESULTS</a>
         </div>
       </div>
     </>
--- src/components/events/klResults.component.jsx
+++ src/components/events/klResults.component.jsx
@@ -18,23 +18,23 @@ function klResults() {
           <h4 className="klResultSubText">CROSS COUNTRY</h4>
         </div>
         <div className='klResultCol'>
-          <a className='klResultButton'>2024 RESULTS</a>
-          <a className='klResultButton'>2023 RESULTS</a>
-          <a className='klResultButton'>2022 RESULTS</a>
-          <a className='klResultButton'>2021 RESULTS</a>
-          <a className='klResultButton'>2019 RESULTS</a>
-          <a className='klResultButton'>2018 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_15944f2a8c894697ac1d4bd03b26221e.pdf' className='klResultButton'>2024 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_655a990a9269438f9d84117be67021f7.pdf' className='klResultButton'>2023 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_fb434fc143044e80ab2ac78b2be27c3d.pdf' className='klResultButton'>2022 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_60350db6b5f5418cb5ebbcad43d4c218.pdf?index=true' className='klResultButton'>2021 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_a350758d463e497fb06cff029d99bc82.pdf?index=true' className='klResultButton'>2019 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_250d9cfcbf184bb1bc973569b47edc9a.pdf?index=true' className='klResultButton'>2018 RESULTS</a>
         </div>
       </div>
 
       <div className='klRow mobileOnly'>
         <div className='klResultCol'>
-          <a className='klResultButton'>2024 RESULTS</a>
-          <a className='klResultButton'>2023 RESULTS</a>
-          <a className='klResultButton'>2022 RESULTS</a>
-          <a className='klResultButton'>2021 RESULTS</a>
-          <a className='klResultButton'>2019 RESULTS</a>
-          <a className='klResultButton'>2018 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_15944f2a8c894697ac1d4bd03b26221e.pdf' className='klResultButton'>2024 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_655a990a9269438f9d84117be67021f7.pdf' className='klResultButton'>2023 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_fb434fc143044e80ab2ac78b2be27c3d.pdf' className='klResultButton'>2022 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_60350db6b5f5418cb5ebbcad43d4c218.pdf?index=true' className='klResultButton'>2021 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_a350758d463e497fb06cff029d99bc82.pdf?index=true' className='klResultButton'>2019 RESULTS</a>
+          <a href='https://www.burlingtontrackandfield.org/_files/ugd/2f2bfd_250d9cfcbf184bb1bc973569b47edc9a.pdf?index=true' className='klResultButton'>2018 RESULTS</a>
         </div>
       </div>
     </>
