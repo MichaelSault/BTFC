05acaa300038a8b644c13b73c96bd99765ea577d
michaelsault <michael_sault@hotmail.com>
2025-04-18 23:42:48 -0400

    simplified the bod component

--- src/components/directors.component.jsx
+++ src/components/directors.component.jsx
@@ -30,34 +30,16 @@ function Directors() {
       <div className="bodTitleRow">
         <h1 className="bodTitle">2025 BOARD OF DIRECTORS</h1>
       </div>
-      <div className="desktopOnly">
-        <div className="bodSubRow">
+      <div className="bodCentered">
           <BodTile name={"Gustav Karlsson"} title={"Club President"} photo={President} />
           <BodTile name={"Scott Douglas"} title={"Athletic Director"} photo={AthleticDirector}/>
           <BodTile name={"Angus Shuttleworth"} title={"Treasurer"} photo={Treasurer}/>
-        </div>
-        <div className="bodSubRow">
           <BodTile name={"Danielle Leahy Laughlin"} title={"Secretary"} photo={Secretary}/>
           <BodTile name={"Tammy MacKenzie"} title={"Head Coach"} photo={Coach}/>
-        </div>
-        <div className="bodSubRow">
           <BodTile name={"Nirup Urs"} title={"Director"} photo={Director1}/>
           <BodTile name={"Heather Larsen"} title={"Director"} photo={Director2}/>
           <BodTile name={"JC Castellanos"} title={"Director"} photo={Director3}/>
           <BodTile name={"David Parkes"} title={"Director"} photo={Director4}/>
-        </div>
-      </div>
-
-      <div className="mobileOnly">
-        <BodTile name={"Gustav Karlsson"} title={"President"} photo={President} />
-        <BodTileRight name={"Scott Douglas"} title={"Athletic Director"} photo={AthleticDirector}/>
-        <BodTile name={"Angus Shuttleworth"} title={"Treasurer"} photo={Treasurer}/>
-        <BodTileRight name={"Danielle Leahy Laughlin"} title={"Secretary"} photo={Secretary}/>
-        <BodTile name={"Tammy MacKenzie"} title={"Head Coach"} photo={Coach}/>
-        <BodTileRight name={"Nirup Urs"} title={"Director"} photo={Director1}/>
-        <BodTile name={"Heather Larsen"} title={"Director"} photo={Director2}/>
-        <BodTileRight name={"JC Castellanos"} title={"Director"} photo={Director3}/>
-        <BodTile name={"David Parkes"} title={"Director"} photo={Director4}/>
       </div>
     </div>
     </>
