6377f2c7f6234d3a7890f770a0fb4d555459dded
michaelsault <michael_sault@hotmail.com>
2025-05-11 22:51:01 -0400

    started updating design for the teams components

--- src/components/programs/distance.component.jsx
+++ src/components/programs/distance.component.jsx
@@ -18,19 +18,22 @@ function Distance() {
         </div>
       
         <div className="teamTextCol">
-          <h1 className="teamName">SENIOR DISTANCE TEAM</h1>
-          <p className="teamText"><b>Ages:</b> Highschool age and up</p>
-          <p className="teamText"><b>Distances:</b> Middle and Long distance (800m and up)</p>
-          <p className="teamText"><b>Experience Level:</b> Should be able to run at least 4km</p>
-          <p className="teamText"><b>Training:</b> 2-3 days per week</p>
-          <p className="teamText"><b>Training Days:</b></p>
-          <ul className="teamText">
-            <li>Monday and Wednesday evenings</li>
-            <li>Saturday mornings</li>
-          </ul>
-          <p className="teamText"><b>Racing:</b> Meets are on some weekends, schedule to come.</p>
+          <div className="teamTextSubCol">
+            <h1 className="teamName">SENIOR DISTANCE TEAM</h1>
+            <p className="teamText"><b>Ages:</b> Highschool age and up</p>
+            <p className="teamText"><b>Distances:</b> Middle and Long distance (800m and up)</p>
+            <p className="teamText"><b>Experience Level:</b> Should be able to run at least 4km</p>
+            <p className="teamText"><b>Training:</b> 2-3 days per week</p>
+            <p className="teamText"><b>Training Days:</b></p>
+            <ul className="teamText">
+              <li>Monday and Wednesday evenings</li>
+              <li>Saturday mornings</li>
+            </ul>
+            <p className="teamText"><b>Racing:</b> Meets are on some weekends, schedule to come.</p>
 
-          <a href='https://www.trackie.com/club/2025-burlington-track-field-club-outdoor-only-waitlist/1018740/' className='teamButton'>REGISTER</a>
+            <a href='https://www.trackie.com/club/2025-burlington-track-field-club-outdoor-only-waitlist/1018740/' className='teamButton'>REGISTER</a>
+          </div>
+          
         </div>
       </div>
     </>
