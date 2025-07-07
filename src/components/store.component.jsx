f7adc3ad66c9af2d37d1d564cb00cacc518033c8
michaelsault <michael_sault@hotmail.com>
2025-07-06 23:07:37 -0400

    rebuilt the image swapper in js using useState to resolve issue with background images in css

--- src/components/store.component.jsx
+++ src/components/store.component.jsx
@@ -1,5 +1,6 @@
 import '../App.css';
 import '../styles/store.css';
+import {useState, useEffect} from 'react';
 
 import item1 from '../assets/BTFC Apparel/hat.png';
 import item2 from '../assets/BTFC Apparel/blackshirt.png';
@@ -7,8 +8,41 @@ import item3 from '../assets/BTFC Apparel/hoodie.png';
 import item4 from '../assets/BTFC Apparel/longshirt.png';
 import item5 from '../assets/BTFC Apparel/yellowshirt.png';
 
+let storeItems = [item1, item2, item3, item4, item5];
+let storeIndex = 0;
+let firstTime = 0;
 
 function Store() {
+
+  const [currentItem, setCurrentItem] = useState({
+   item: ""
+  });
+
+  useEffect(() => {
+    console.log("useEffect run");
+    setCurrentItem(storeItems[0]);
+    if (firstTime == 0) {
+      firstTime = 1;
+      resetTimer();
+    }
+  }, []);
+
+  function resetTimer() {
+    const timeoutId = setTimeout(() => {
+      console.log('It has been 10 seconds');
+      changeItem();
+    }, 10000);
+  }
+
+  function changeItem() {
+    storeIndex > 3 ? storeIndex = 0 : storeIndex++;
+    console.log(storeIndex);
+    console.log(storeItems[storeIndex]);
+    setCurrentItem(storeItems[storeIndex]);
+    console.log(currentItem);
+    resetTimer();
+  }
+
 
   return (
     <>
@@ -24,7 +58,7 @@ function Store() {
               <div className='outerwrapperStore'>
                 <div className='innerwrapperStore'>
                   <div className='sliderContainer'>
-                    <div className='pic'/>
+                    <img className="storeItems" src={currentItem}/>
                   </div>
                 </div>
               </div>
@@ -35,7 +69,7 @@ function Store() {
           <div className='storeCol'>
             <h1 className='storeTitle'>BTFC APPAREL STORE</h1>
             <div className='sliderContainer'>
-              <div className='pic'/>
+              <img className="storeItems" src={currentItem}/>
             </div>
             <p className='storeDescription'>The BTFC apparel store is powered by KCK Teamwear.  All orders are made on demand and could take 7-10 business dats to complete.</p>
             <div className='buttonRowStore'>
