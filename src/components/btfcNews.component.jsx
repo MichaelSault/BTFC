984eb47faeecdedb227438f1562b6c76471ac087
michaelsault <michael_sault@hotmail.com>
2026-01-15 00:14:38 -0500

    added a news section with current athlete of the year winner

--- src/components/btfcNews.component.jsx
+++ src/components/btfcNews.component.jsx
@@ -8,30 +8,30 @@ function BTFCNews() {
 
   return (
     <>
-      <title>Race Results | Burlington Track & Field Club</title>
+      <title>BTFC News | Burlington Track & Field Club</title>
       <meta name='description' content='Stay updated with the latest race results from Burlington Track & Field. Explore our comprehensive listings to track your progress and celebrate achievements. Visit us now!'/>
-      <div className='newsRow desktopOnly'>
+      <div className='newsRow desktopOnlyNews'>
         <div className='newsCol2'>
           <div className="outerwrapperNews">
-            <img className="newsImage" src={awardPhoto} alt="BTFC Appearel options availaible in the store."/>
+            <img className="newsImage" src={awardPhoto} alt="2025 Athlete of the Year Winner.  Female U18.  Maya Markowska, Burlington Track & Field Club."/>
           </div>
         </div>
         <div className='newsCol'>
-          <h1 className='newsTitle'>KEN LAKE MEMORIAL MEET</h1>
-          <p className='resultDescription'>Every year, the Burlington Track and Field Club hosts the Ken Lake Memorial Meet during the Cross Country season.</p>
+          <h1 className='newsTitle'>Congratulations!</h1>
+          <p className='newsDescription'>Congratulations Maya Markowska on being named Athletic's Ontario's 2025 Female U18 Athlete of the Year!</p>
           <div className='buttonRowNews'>
-            <a href='/#/KenLake' className='newsButton'>RESULTS</a>
+            <a href='https://www.instagram.com/p/DSvfeKziZIS/?img_index=3' target='_blank' className='newsButton'>VIEW POST</a>
           </div>
         </div>
       </div>
 
-      <div className='newsRow mobileOnly'>
+      <div className='newsRow mobileOnlyNews'>
         <div className='newsCol'>
-          <h1 className='newsTitle'>KEN LAKE MEMORIAL MEET</h1>
-          <img className='eventImage' src={awardPhoto} alt='An action shot from the Ken Lake Memorial Cross Country Meet' />
-          <p className='resultDescription'>Every year, the Burlington Track and Field Club hosts the Gord Rusby Memorial Meet during the Outdoor Track season.</p>
+          <h1 className='newsTitle'>Congratulations!</h1>
+          <img className='newsImageMobile' src={awardPhoto} alt="2025 Athlete of the Year Winner.  Female U18.  Maya Markowska, Burlington Track & Field Club." />
+          <p className='newsDescription'>Congratulations Maya Markowska on being named Athletic's Ontario's 2025 Female U18 Athlete of the Year!</p>
           <div className='buttonRowNews'>
-            <a href='/#/KenLake' className='newsButton'>RESULTS</a>
+            <a href='https://www.instagram.com/p/DSvfeKziZIS/?img_index=3' target='_blank' className='newsButton'>VIEW POST</a>
           </div>
         </div>
       </div>
