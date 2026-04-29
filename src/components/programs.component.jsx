6dc1eb27b45c90ffb2689b96ebd881d6144195dd
michaelsault <michael_sault@hotmail.com>
2026-04-29 00:11:40 -0400

    added an alert to notify the viewer that we are at full capacity

--- src/components/programs.component.jsx
+++ src/components/programs.component.jsx
@@ -8,7 +8,15 @@ function Programs() {
     <>
       <title>Programs | Burlington Track and Field Club</title>
       <meta name="description" content="Discover the diverse programs offered by Burlington Track and Field, catering to runners of all levels and ages. From training sessions to competitive events, find your perfect fit with us today!"/>
+      
       <div className="programsRow">
+        <div className="alert">
+          <span className="closebtn" onClick={(e) => {e.target.parentElement.style.display="none";}}>&times;</span> 
+          <strong>Jr. Registration Update: Outdoor Season At Full Capacity</strong> <br/><br/>
+          Unfortunately, registration for our <b>Outdoor Season</b> filled up much faster than expected. We have already taken on more members than initially anticipated and, as a result, we are <b>strictly at full capacity.  Note:</b> We are unable to accept any late registrations at this time.<br/><br/>
+          If you missed out this time, we would love to have you join us for our upcoming <b>Cross Country Season.</b> Registration Opens: August 4 (Pending availability)<br/><br/>
+          Thank you for your incredible interest and support of our programs!
+        </div>
         <h1 className='programTitle'>BTFC PROGRAMS</h1>
         <p className='description'>The Burlington Track and Field Club offers both Junior and Senior programs. Specialized distance and sprint programs are offered at the senior level.  Please see the links below for more details.<br/><br/></p>
         
